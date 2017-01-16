ddc3338c5310ce4d81c86772d9802351d1cbb6de
Dan Wolanski <wolanskd@yahoo.com>
2017-01-16 12:44:15 -0500

    Added REST port 8181

--- setfirewall.sh
+++ setfirewall.sh
@@ -4,30 +4,34 @@ firewall-cmd --zone=public --add-port=5060/tcp --permanent
 echo -n "Sip ports 5060/udp - "
 firewall-cmd --zone=public --add-port=5060/udp --permanent
 
-echo -n "Audio RTP/RTCP ports 49152-53152/udp - "
-firewall-cmd --zone=public --add-port=49152-53152/udp --permanent
-echo -n "Video RTP/RTCP ports 57344-61344/udp - " 
-firewall-cmd --zone=public --add-port=57344-61344/udp --permanent
+# TODO: set this for port range on RTP Proxy
+#echo -n "Audio RTP/RTCP ports 49152-53152/udp - "
+#firewall-cmd --zone=public --add-port=49152-53152/udp --permanent
+#echo -n "Video RTP/RTCP ports 57344-61344/udp - " 
+#firewall-cmd --zone=public --add-port=57344-61344/udp --permanent
 
 echo -n "https for webui 443-444/tcp - "
 firewall-cmd --zone=public --add-port=443-444/tcp --permanent
 
-echo -n "WebRTC port: 1080/tcp - "
-firewall-cmd --zone=public --add-port=1080/tcp --permanent
-echo -n "MSRP default port 2855/tcp" - 
-firewall-cmd --zone=public --add-port=2855/tcp --permanent
+#echo -n "WebRTC port: 1080/tcp - "
+#firewall-cmd --zone=public --add-port=1080/tcp --permanent
+#echo -n "MSRP default port 2855/tcp" - 
+#firewall-cmd --zone=public --add-port=2855/tcp --permanent
 echo -n "REST service port 81/tcp -"
 firewall-cmd --zone=public --add-port=81/tcp --permanent
-echo -n "ports 56000-56999/udp -"
-firewall-cmd --zone=public --add-port=56000-56999/udp --permanent
+echo -n "REST service port 8181/tcp -"
+firewall-cmd --zone=public --add-port=8181/tcp --permanent
+
+#echo -n "ports 56000-56999/udp -"
+#firewall-cmd --zone=public --add-port=56000-56999/udp --permanent
 
 
 
 echo -n "SNMP Port 161-162/udp - "
 firewall-cmd --zone=public --add-port=161-162/udp --permanent
 
-echo -n "Web/REST Admin https 10443/tcp - "
-firewall-cmd --zone=public --add-port=10443/tcp --permanent
+#echo -n "Web/REST Admin https 10443/tcp - "
+#firewall-cmd --zone=public --add-port=10443/tcp --permanent
 
 #MRB ports
 echo -n "MRB adapter ports 12000-12010/tcp - "
